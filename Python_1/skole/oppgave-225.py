83e77b8a88bcdacd961e08107ab0d38d63b15bc7
LifeOnTheLine <168783878+Springo2008@users.noreply.github.com>
2025-10-20 11:27:31 +0200

    Create oppgave-225.py

---
+++ Python_1/skole/oppgave-225.py
@@ -0,0 +1,9 @@
+import numpy as np
+def f(x):
+    return ((2*x**2)-x+11)/((x**3)-8)
+
+# lim x-> inf f(x)
+x_values = np.linspace(100, 1000, num=10)
+f_values = f(x_values)
+limit_inf = f_values[-1]
+print(f"Lim x-> inf f(x) = {limit_inf}")
