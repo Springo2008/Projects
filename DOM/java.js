f97dad580b6b075a598da8d777676d0ee4f2fd13
LifeOnTheLine <168783878+Springo2008@users.noreply.github.com>
2026-01-15 15:32:20 +0100

    Enhance notepad application with improved UI and functionality, including character and word count display, text locking feature, and updated save functionality.

--- DOM/java.js
+++ DOM/java.js
@@ -1,10 +1,42 @@
 function saveText() {
-    var text = document.getElementById('textArea').value;
-    var title = document.getElementById('textTitle').value;
+    var textEl = document.getElementById('textArea').value;
+    var titleEl = document.getElementById('textTitle').value;
     var downloadLink = document.createElement('a');
-    downloadLink.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    downloadLink.download=title+".txt";
+    downloadLink.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(textEl));
+    downloadLink.download=titleEl+".txt";
     document.body.appendChild(downloadLink);
     downloadLink.click();
-    document.body.removeChild(downloadLink)
+    document.body.removeChild(downloadLink);
 }   
+const textarea = document.getElementById("textArea");
+textarea.addEventListener("input", function() {
+    let total_length = this.value.length;
+    let word_count = this.value.trim().split(/\s+/).length;
+    document.getElementById("char-length").innerText = total_length +" Letters, "+ word_count +" Words";
+})
+
+
+
+
+function disableTxt() {
+  var checkbox = document.getElementById("check");
+  var text = document.getElementById("textArea");
+
+  if (checkbox.checked){
+    text.disabled = true;
+  } else{
+    text.disabled = false;
+   }
+}
+  
+
+
+/*function undisableTxt() {
+  
+  
+}
+}
+
+);
+*/
+
